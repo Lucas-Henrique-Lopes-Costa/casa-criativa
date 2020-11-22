51a6247adb34793e205892ae2cfb7415da70dccd
LucasH2o <lucass20586@gmail.com>
2020-11-22 19:04:01 -0300

    Update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,7 @@
-/* Acessando uma Tag */
-
-document
-    .querySelector(".fat") /* Encontra o botão "fat" */
-    .addEventListener("click", function () {
-        /* Quando clikar, ele executa a função */
-        document
-            .querySelector("#footer")
-            .classList
-            .toggle("hide") /* Adiciona a classe "hide" que entra em uma estilização */
-    })
+function onOff () {
+    /* Quando clikar, ele executa a função */
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide") /* Adiciona a classe "hide" que entra em uma estilização */
+}
